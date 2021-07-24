f7a68aac1719adce878a934823a9c648cefb4cd3
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-07-24 20:48:05 +0100

    Adde right and left sensors

--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/debug_distance_sensors.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/debug_distance_sensors.py
@@ -1,11 +1,14 @@
 from gpiozero import DistanceSensor, LED
 from signal import pause
 
-sensor = DistanceSensor(echo=5, trigger=6, max_distance=1, threshold_distance=0.2)
-led = LED(16)
+left_sensor = DistanceSensor(echo=5, trigger=6, max_distance=1, threshold_distance=0.1)
+right_sensor = DistanceSensor(echo=13, trigger=26, max_distance=1, threshold_distance=0.11)
 
-sensor.when_in_range = lambda: print("Obstacle in range")
-sensor.when_out_of_range = lambda: print("Obstacle out of range")
+left_sensor.when_in_range = lambda: print("Left obstacle in range")
+left_sensor.when_out_of_range = lambda: print("Left obstacle out of range")
+
+right_sensor.when_in_range = lambda: print("Right obstacle in range")
+right_sensor.when_out_of_range = lambda: print("Right obstacle out of range")
 
 
 pause()
