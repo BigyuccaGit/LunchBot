0b7cafc2532d6ba74d25b31c32605037391ed226
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-07-27 20:29:05 +0100

    distance threshold set

--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/debug_distance_sensors.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/debug_distance_sensors.py
@@ -1,9 +1,13 @@
 from gpiozero import DistanceSensor, LED
 from signal import pause
 
-left_sensor = DistanceSensor(echo=5, trigger=6, max_distance=1, threshold_distance=0.1)
-right_sensor = DistanceSensor(echo=13, trigger=26, max_distance=1, threshold_distance=0.11)
+threshold_distancecm=11 # cm
 
+threshold_distance = threshold_distancecm/100
+
+left_sensor = DistanceSensor(echo=5, trigger=6, max_distance=1, threshold_distance=threshold_distance)
+right_sensor = DistanceSensor(echo=13, trigger=26, max_distance=1, threshold_distance=threshold_distance)
+ 
 left_sensor.when_in_range = lambda: print("Left obstacle in range")
 left_sensor.when_out_of_range = lambda: print("Left obstacle out of range")
 
