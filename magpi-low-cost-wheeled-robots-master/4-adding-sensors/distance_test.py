16185abda3938a26b2df03167977a1642d1c5137
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-07-30 23:37:11 +0100

    Changed to source/value approach

--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/distance_test.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/distance_test.py
@@ -1,8 +1,35 @@
 from gpiozero import DistanceSensor
 from time import sleep
+from gpiozero import LED
+from gpiozero.tools import booleanized
+from signal import pause
 
-sensor = DistanceSensor(echo=5,trigger=6)
+threshold_distance=0.3
+ 
+lsensor = DistanceSensor(echo=5,  trigger=6,  threshold_distance=threshold_distance, queue_len=4)
+rsensor = DistanceSensor(echo=13, trigger=26, threshold_distance=threshold_distance, queue_len=4)
+ledl=LED(12)
+ledr=LED(16)
 
-while True:
-    print('Distance to nearest object is', sensor.distance * 100, 'cm')
-    sleep(1)
+#while True:
+
+#    print('Distance to nearest l object is', lsensor.distance*100, "cm ", lsensor.value)
+#    if lsensor.value < threshold_distance:
+#        ledl.on()
+#    else:
+#        ledl.off()
+
+#    ledl.value=lsensor.value < threshold_distance
+
+#    print('Distance to nearest r object is', rsensor.distance*100, 'cm ', rsensor.value)
+#    if rsensor.value < threshold_distance:
+#        ledr.on()
+#    else:
+#        ledr.off()
+#    ledr.value=rsensor.value < threshold_distance
+#    sleep(0.01)
+
+ledl.source=booleanized(lsensor,0,threshold_distance)
+ledr.source=booleanized(rsensor,0,threshold_distance)
+
+pause()
