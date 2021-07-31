2ccc3d66cb0b7ca42073d192a68faf160e687bee
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-07-31 20:36:56 +0100

    1st attempt at full distance avoid code

--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/distance_avoid.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/distance_avoid.py
@@ -1,19 +1,25 @@
 from signal import pause
 import atexit
 import gpiozero
-from gpiozero.tools import scaled, negated
+from gpiozero.tools import scaled, negated, booleanized
 
+# Parameters
 speed = 0.5
+threshold_distance=0.2 #meters
 
+# Instantiate robot
 robot = gpiozero.Robot(left=(27, 17), right=(24, 23))
  
-left_obstacle_sensor = gpiozero.DistanceSensor(echo=5, trigger=6, max_distance=1, threshold_distance=0.2)
-right_obstacle_sensor = gpiozero.DistanceSensor(echo=13, trigger=26, max_distance=1, threshold_distance=0.2)
+# Instantiate sensors
+left_distance_sensor = gpiozero.DistanceSensor(echo=5, trigger=6, max_distance=1, threshold_distance=0.2)
+right_distance_sensor = gpiozero.DistanceSensor(echo=13, trigger=26, max_distance=1, threshold_distance=0.2)
 
 # Ensure it will stop
 atexit.register(robot.stop)
 
-robot.right_motor.source = scaled(left_obstacle_sensor, -speed, speed)
-robot.left_motor.source = scaled(right_obstacle_sensor, -speed, speed)
+# Source/value setup so opposite motor reverses when object detected, thus robot turns away
+robot.right_motor.source = scaled(negated(booleanized(left_distance_sensor,0,threshold_distance)), -speed, speed)
+robot.left_motor.source = scaled(negated(booleanized(right_distance_sensor,0,threshold_distance)), -speed, speed)
 
+# Wait for something to happen
 pause()
