a794c00c7e66399717352675681dc6ca399c4824
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-08-26 14:12:03 +0100

    Fixed typos and corrected order of spin selection

--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/distance_new.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/distance_new.py
@@ -16,7 +16,6 @@ hysteresis_cm=2
 threshold_distance=threshold_distance_cm/100.0
 hysteresis=hysteresis_cm/100.0
 
-
 factory = PiGPIOFactory()
 
 # Instantiate robot
@@ -28,67 +27,66 @@ l_sensor = gpiozero.DistanceSensor(echo=13, trigger=26, max_distance=1, threshol
 
 # Set up main operations
 
-def backwards:
+def backwards():
   robot.backward()
   sleep(1.0)
   robot.stop()    
   
 def right_spin():
+  print("RIGHT SPIN")
   robot.left_motor.forward()
   robot.right_motor.backward()
   sleep(1.0)
   robot.stop()
   
 def left_spin():
+  print("LEFT SPIN")
   robot.right_motor.forward()
   robot.left_motor.backward()
   sleep(1.0)
   robot.stop()
   
-coin_toss=[left_spin, right_spin)]
+coin_toss=[left_spin, right_spin]
 def random_spin():
   coin_toss[random.randint(0,1)]()
 
-spin_options=[random_spin, left_spin, right_spin, random_spin]
+spin_options=[random_spin, right_spin, left_spin, random_spin]
+#              failsafe,   left sensor, right sensor, both sensors
 def choose_spin(r_in_range, l_in_range):
-  option = r_in_range << 1 + l_in_range
+  option = (r_in_range << 1) + l_in_range
   #spin_option[option]()
-  return option
+  print(r_in_range, l_in_range, option)
+  return spin_options[option]
   
 # Ensure it will stop
 atexit.register(robot.stop)
 
-# Wait for something to happen
-
+# Start main Loop forever
 while True:
+
+  # Sent robot forward
   print("FORWARD")
   robot.forward()
   print(r_sensor.in_range, r_sensor.distance*100, l_sensor.in_range, l_sensor.distance*100)
 
+  # Wait for object to come in range
   while not r_sensor.in_range and not l_sensor.in_range: 
   
   #  print(r_sensor.in_range, r_sensor.distance*100, l_sensor.in_range, l_sensor.distance*100)
     sleep(0.01) 
 
-  spin_option=choose_spin((r_sensor_in_range, l_sensor_in_range)
-  print("STOP")
+  # Stio the robot
   robot.stop()
+  print("STOP")
 
+  # Choose which spin option to do in a moment based on current settings
+  spin_option=choose_spin(r_sensor.in_range, l_sensor.in_range)
+
+  # Go Backwards
   print("BACKWARD")
-  backwards()
-  #robot.backward()
-  #sleep(1.0)
-  #robot.stop()  
+  backwards()  
 
+  # Execute chosen spin option
   print("SPIN")
-  #random_spin()
   spin_option()
-  #coin=random.randint(0,1)
-  #if coin == 0: 
-  #  robot.left_motor.forward()
-  #  robot.right_motor.backward()
-  #else:
-  #  robot.right_motor.forward()
-  #  robot.left_motor.backward()
-  #sleep(1.0)
-  #robot.stop()
+  
