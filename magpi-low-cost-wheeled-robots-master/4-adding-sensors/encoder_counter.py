aad87c5be113e95afcfa15b15abb0a9f0ac482bb
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-10-18 19:14:07 +0100

    Added check for -ve times

--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/encoder_counter.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/encoder_counter.py
@@ -1,50 +1,36 @@
 from gpiozero import DigitalInputDevice
-import time
-import threading
+import atexit
+from background_task import BackgroundTask
 
 class EncoderCounter():
-    class BackgroundTasks(threading.Thread):
-        def __init__(self, func, *args, **kwargs):
-            super().__init__(*args,**kwargs)
-            self.func = func
-            
-        def run(self,*args,**kwargs):
-          t=time.time()
-          while True:
-              t += 0.1
-              self.func()
-            #  print('Hello', time.time())
-              time.sleep(t-time.time())
               
-    def calc_delta(self):
-        self.pulse_delta = self.pulse_count - self.pulse_last
-        self.pulse_last = self.pulse_count    
- 
-    def __init__(self, pin_number):
+    def __init__(self, pin_number, id):
         self.pulse_count = 0
         self.device = DigitalInputDevice(pin=pin_number)
-        self.device.pin.when_changed = self.when_changed
+        self.device.pin.edges='rising'
         self.direction = 1
-        self.last=0
-        self.pulse_last=0
-        self.record=False
-        self.list=[]
+        self.pulse_last=-1
         self.pulse_delta=0
-        #print(dir(self.device.pin))
-        self.last_pulse_count=0
-
-        self.t = EncoderCounter.BackgroundTasks(self.calc_delta, daemon=True)
+        self.id = id
+        
+        self.device.pin.when_changed = self.when_changed
+        self.t = BackgroundTask(self.calc_delta, self.id)
         self.t.start()
-
+        
+    def calc_delta(self):
+        self.pulse_delta = self.pulse_count - self.pulse_last
+  #      print(self.id, self.pulse_delta, self.pulse_count, self.pulse_last)
+        self.pulse_last = self.pulse_count    
+       
     def when_changed(self, time_ticks, state):
-        time_diff=time_ticks-self.last
-        self.last=time_ticks
-  #      if self.record :
-  #          self.list.append(time_diff)
         self.pulse_count += self.direction
 
     def set_direction(self, direction):
         self.direction = direction
+
+    def stop_all(self):
+        self.device.pin.when_changed = None
+        
 
        
 
--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/encoder_time_test.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/encoder_time_test.py
@@ -7,74 +7,42 @@ from encoder_counter import EncoderCounter
 import time
 import logging
 
+def exit_all():
+  motorr.stop()
+  motorl.stop()
+     
+
 logger = logging.getLogger("test_encoders")
 
+motorl = Motor(forward=24, backward=23)
 motorr = Motor(forward=27, backward=17)
-#motorl = Motor(forward=23, backward=24)
 
-rotorr = EncoderCounter(7)
-print(rotorr.device.pin.edges)
-rotorr.device.pin.edges='rising'
-print(rotorr.device.pin.edges)
+rotorl = EncoderCounter(8,'L')
+rotorr = EncoderCounter(7,'R')
+
+print(rotorl.device.pin.edges, rotorr.device.pin.edges )
+
 #quit()
 
 speed = 1
+atexit.register(exit_all)
 
 motorr.forward(speed)
-#motorl.forward(speed)
+motorl.forward(speed)
 
 sleep(0.1)
-
-
-#rotorl = EncoderCounter(8)
-
-atexit.register(motorr.stop)
 
 sample=0
 
 start_time=time.time()
 stop_at_time=start_time+20
-true_start=start_time+0.5
-true_end=start_time+5.5
-#while (sample <= 100):
+
 while time.time() < stop_at_time:
-
-    #if sample == 50:
-#    now=time.time()
-#   rotorr.record=now >= true_start and now < true_end
-#        rotorr.record=True
-
-#    if sample == 51:
-#        rotorr.record=False
-
-#    print("Steps = ", rotor.steps, rotor.value, sample)
-#    rotorr.pulse_count
-
-   # print("Steps = ", thisr, thisl, thisr-last, status, sample)
- #   sample = sample+1
-     print(time.time(), rotorr.pulse_count, rotorr.pulse_delta)
+#     print(time.time(), rotorl.pulse_count, rotorl.pulse_last, rotorl.pulse_delta, rotorr.pulse_count, rotorr.pulse_last, rotorr.pulse_delta)
+#     print(time.time(), rotorr.pulse_count, rotorr.pulse_delta)
+     print(time.time(), rotorl.pulse_delta, rotorr.pulse_delta)
      sleep(0.1)
 
 
 motorr.stop()
-#motorl.stop()
-
-#print(rotorr.list)
-#print("Summary", forward, backward, stopped)
-
-
-#[print((x)) for x in rotorr.list]
-
-#sum=0
-#for x in rotorr.list:
-#    sum+=x
-    
-#print("sum", sum, rotorr.pulse_count)
-
-
-#sum/=len(rotorr.list)
-
-#rotorr.list
-#print("Mean", sum)
-
-#quit()
+motorl.stop()
