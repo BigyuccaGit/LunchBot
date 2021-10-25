bcea39d05265e060f28cbe948cead6aa4b7daa67
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-10-25 14:56:19 +0100

    Logged to disk; Better closing

--- magpi-low-cost-wheeled-robots-master/4-adding-sensors/encoder_time_test.py
+++ magpi-low-cost-wheeled-robots-master/4-adding-sensors/encoder_time_test.py
@@ -7,14 +7,23 @@ from gpiozero import DistanceSensor, DistanceSensorNoEcho
 from encoder_counter import EncoderCounter
 import time
 import logging
+import sys
+#import vpython as vp
 
 def exit_all():
   motorr.stop()
   motorl.stop()
   sleep(1.0)
-     
+  
+file_handler = logging.FileHandler(filename='test_encoder.log',mode='w')
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [file_handler, stdout_handler]
 
-logger = logging.getLogger("test_encoders")
+logging.basicConfig(level=logging.INFO, handlers=handlers, format='%(message)s')
+
+  
+
+logger = logging.getLogger("test_encoder")
 
 motorl = Motor(forward=24, backward=23)
 motorr = Motor(forward=27, backward=17)
@@ -37,29 +46,37 @@ print(rotorl.device.pin.edges, rotorr.device.pin.edges )
 speed = 1
 atexit.register(exit_all)
 
+
+
+sample=0
+
+
+start_time=time.time()
+stop_at_time=start_time+5
+
+#print("vp.graph")
+#vp.graph(xmin=0, xmax=10, scroll=True)
+#print("temp_graph")
+#temp_graph=vp.gcurve()
+
 motorr.forward(speed)
 motorl.forward(speed)
 
 sleep(0.1)
 
-sample=0
-
-start_time=time.time()
-stop_at_time=start_time+5
-
+t=time.time()
 while time.time() < stop_at_time:
+     t = t + 0.1
 #     print(time.time(), rotorl.pulse_count, rotorl.pulse_last, rotorl.pulse_delta, rotorr.pulse_count, rotorr.pulse_last, rotorr.pulse_delta)
 #     print(time.time(), rotorr.pulse_count, rotorr.pulse_delta)
-     print(f"{time.time():.2f}\
-{rotorl.pulse_delta:10d} {rotorl.pulse_delta2:4d}\
-{rotorl.smoothed_pulse_delta:6d} {rotorl.smoothed_pulse_delta2:4d}\
-{lsensor.distance*100:6.1f}\
-{rotorr.pulse_delta:10d} {rotorl.pulse_delta2:4d}\
-{rotorr.smoothed_pulse_delta:6d} {rotorr.smoothed_pulse_delta2:4d}\
-{rsensor.distance*100:6.1f}\
-"\
-           )
-     sleep(0.1)
+     elapsed = time.time()-start_time
+#     print( f"dummy {elapsed}")
+     logger.info(f"{elapsed:.2f} {rotorl.pulse_delta:10d} {rotorl.pulse_delta2:4d} {rotorl.smoothed_pulse_delta:6d} {rotorl.smoothed_pulse_delta2:4d} {lsensor.distance*100:6.1f} {rotorr.pulse_delta:10d} {rotorl.pulse_delta2:4d} {rotorr.smoothed_pulse_delta:6d} {rotorr.smoothed_pulse_delta2:4d} {rsensor.distance*100:6.1f}")
+           
+ #    vp.rate(10)
+ #    temp.graph.plot(elapsed, smoothed_pulse_delta)
+  #   sleep(0.2)
+     sleep(max(0,t - time.time()))
 
 exit_all()
 
