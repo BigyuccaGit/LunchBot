867053e4d48f99bbebf053fb194ae8cc68767b0f
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-11-06 14:36:18 +0000

    Added control_server and robot_modes

--- server/robot_modes.py
+++ server/robot_modes.py
@@ -28,19 +28,32 @@ class RobotModes(object):
 #        "face_track": "face_track_behavior.py",
     }
 
+#    menu_config = [
+#        {"mode_name": "avoid_behavior", "text": "Avoid Behavior"},
+#        {"mode_name": "circle_head", "text": "Circle Head"},
+#        {"mode_name": "test_rainbow", "text": "LED Rainbow"},
+#        {"mode_name": "test_leds", "text": "Test LEDs"},
+#        {"mode_name": "line_following", "text": "Line Following"},
+#        {"mode_name": "color_track", "text": "Color Tracking"},
+#        {"mode_name": "face_track", "text": "Face Tracking"},
+#        {"mode_name": "manual_drive", "text": "Drive Manually"},
+#        {"mode_name": "behavior_line", "text": "Drive In A Line"},
+#        {"mode_name": "drive_north", "text": "Drive North"}
+#    ]
     menu_config = [
-        {"mode_name": "avoid_behavior", "text": "Avoid Behavior"},
-        {"mode_name": "circle_head", "text": "Circle Head"},
-        {"mode_name": "test_rainbow", "text": "LED Rainbow"},
-        {"mode_name": "test_leds", "text": "Test LEDs"},
-        {"mode_name": "line_following", "text": "Line Following"},
-        {"mode_name": "color_track", "text": "Color Tracking"},
-        {"mode_name": "face_track", "text": "Face Tracking"},
-        {"mode_name": "manual_drive", "text": "Drive Manually"},
-        {"mode_name": "behavior_line", "text": "Drive In A Line"},
-        {"mode_name": "drive_north", "text": "Drive North"}
-    ]
+        {"mode_name": "buzzer", "text": "Sound Buzzer"},
+        {"mode_name": "left_led", "text": "Left LED"},
+        {"mode_name": "right_led", "text": "Right LED"},
+        {"mode_name": "backwards", "text": "Backwards"},
+#        {"mode_name": "line_following", "text": "Line Following"},
+#       {"mode_name": "color_track", "text": "Color Tracking"},
+#        {"mode_name": "face_track", "text": "Face Tracking"},
+#        {"mode_name": "manual_drive", "text": "Drive Manually"},
+#        {"mode_name": "behavior_line", "text": "Drive In A Line"},
+#        {"mode_name": "drive_north", "text": "Drive North"}
+   ]
 
+    # Set up the current process
     def __init__(self):
         self.current_process = None
 
@@ -58,6 +71,7 @@ class RobotModes(object):
         script = self.mode_config[mode_name]['script']
         self.current_process = subprocess.Popen(["python3", script])
 
+    # Stop a process
     def stop(self):
         if self.is_running():
             # Sending the signal sigint is (on Linux) similar to pressing ctrl-c.
@@ -65,5 +79,6 @@ class RobotModes(object):
             self.current_process.send_signal(subprocess.signal.SIGINT)
             self.current_process = None
 
+    # Should we redirect?
     def should_redirect(self, mode_name):
         return self.mode_config[mode_name].get('server') is True and self.is_running()
