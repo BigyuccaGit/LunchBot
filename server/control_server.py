53ae1b57c2a771106d63310a5ae8ab4914bf3904
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-11-06 14:45:10 +0000

    Added render capability

--- server/control_server.py
+++ server/control_server.py
@@ -1,5 +1,4 @@
-# Scrpt to act as a control web service using Flask
-from flask import Flask
+from flask import Flask, render_template, jsonify
 from robot_modes import RobotModes
 
 # A Flask App contains all its routes.
@@ -8,20 +7,33 @@ app = Flask(__name__)
 # Prepare our robot modes for use
 mode_manager = RobotModes()
 
-# Route to run the selected app
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template('menu.html', menu=mode_manager.menu_config)
+
+
 @app.route("/run/<mode_name>", methods=['POST'])
 def run(mode_name):
     # Use our robot app to run something with this mode_name
     mode_manager.run(mode_name)
-    # Confirming message
-    return "%s running"
+    response = {'message': f'{mode_name} running'}
+    if mode_manager.should_redirect(mode_name):
+        response['redirect'] = True
+    return jsonify(response)
 
-# Route to support the stop running process operation
+
 @app.route("/stop", methods=['POST'])
 def stop():
     # Tell our system to stop the mode it's in.
     mode_manager.stop()
-    return "Stopped"
+    return jsonify({'message': "Stopped"})
 
 # Start the server
 app.run(host="0.0.0.0", debug=True)
