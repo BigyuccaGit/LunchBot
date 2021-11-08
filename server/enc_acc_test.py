918f22db84140c404a9da2477787e0821a2b2ee6
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-11-08 15:09:32 +0000

    Added encoder scripts

---
+++ server/enc_acc_test.py
@@ -0,0 +1,88 @@
+from gpiozero import Motor
+from time import sleep
+import atexit
+#from gpiozero import DigitalInputDevice
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import DistanceSensor, DistanceSensorNoEcho
+from encoder_counter import EncoderCounter
+from icm20948 import ICM20948
+import time
+import logging
+import sys
+from pins import *
+
+#import vpython as vp
+
+def exit_all():
+  motorr.stop()
+  motorl.stop()
+  sleep(1.0)
+  
+file_handler = logging.FileHandler(filename='test_encoder.log',mode='w')
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [file_handler, stdout_handler]
+
+logging.basicConfig(level=logging.INFO, handlers=handlers, format='%(message)s')
+
+  
+
+logger = logging.getLogger("test_encoder")
+
+motorl = Motor(forward=L_MOTOR_F_PIN, backward=L_MOTOR_B_PIN)
+motorr = Motor(forward=R_MOTOR_F_PIN, backward=R_MOTOR_B_PIN)
+
+rotorl = EncoderCounter(R_ENCODER_PI,'L')
+rotorr = EncoderCounter(L_ENCODER_PIN,'R')
+
+factory = PiGPIOFactory()
+ 
+lsensor = DistanceSensor(echo=L_SENSOR_ECHO_PIN, trigger=L_SENSOR_TRIGGER_PIN, queue_len=2,
+                                                   pin_factory=factory)
+rsensor = DistanceSensor(echo=R_SENSOR_ECHO_PIN, trigger=R_SENSOR_TRIGGER_PIN, queue_len=2,
+                                                    pin_factory=factory)
+imu = ICM20948()
+
+print(rotorl.device.pin.edges, rotorr.device.pin.edges )
+
+#quit()
+
+speed = 1
+atexit.register(exit_all)
+
+
+
+sample=0
+
+
+start_time=time.time()
+stop_at_time=start_time+5
+
+#print("vp.graph")
+#vp.graph(xmin=0, xmax=10, scroll=True)
+#print("temp_graph")
+#temp_graph=vp.gcurve()
+
+motorr.forward(speed)
+motorl.forward(speed)
+
+sleep(0.1)
+
+t=time.time()
+while time.time() < stop_at_time:
+     t = t + 0.1
+#     print(time.time(), rotorl.pulse_count, rotorl.pulse_last, rotorl.pulse_delta, rotorr.pulse_count, rotorr.pulse_last, rotorr.pulse_delta)
+#     print(time.time(), rotorr.pulse_count, rotorr.pulse_delta)
+     elapsed = time.time()-start_time
+
+     ax, ay, az, gx, gy, gz = imu.read_accelerometer_gyro_data()
+     amag=sqrt(ax*ax + ay*ay + az*az)
+
+     logger.info(f"{elapsed:.2f} {rotorl.pulse_delta:10d} {rotorl.pulse_delta2:4d} {rotorl.smoothed_pulse_delta:6d} {rotorl.smoothed_pulse_delta2:4d} {lsensor.distance*100:6.1f} {rotorr.pulse_delta:10d} {rotorl.pulse_delta2:4d} {rotorr.smoothed_pulse_delta:6d} {rotorr.smoothed_pulse_delta2:4d} {rsensor.distance*100:6.1f {amag:5.2f} {ax:5.2f} {ay:5.2f} {az:5.2f} }")
+           
+ #    vp.rate(10)
+ #    temp.graph.plot(elapsed, smoothed_pulse_delta)
+  #   sleep(0.2)
+     sleep(max(0,t - time.time()))
+
+exit_all()
+
---
+++ server/encoder_counter.py
@@ -0,0 +1,86 @@
+from gpiozero import DigitalInputDevice
+import atexit
+from background_task import BackgroundTask
+
+class EncoderCounter():
+              
+    def __init__(self, pin_number, id, interval=0.1, buffer_delta_size=5, buffer_delta2_size=5):
+        # Initiate encoder and only trigger on rising edge
+        self.device = DigitalInputDevice(pin=pin_number)
+        self.device.pin.edges='rising'
+
+        # Some starting values
+        self.pulse_count = 0
+        self.direction = 1
+        self.pulse_last = 0
+        self.pulse_delta = 0
+        self.pulse_delta_last = 0
+        self.pulse_delta2=0
+
+        # Set up smoothing buffers
+        self.buffer_delta_size = buffer_delta_size
+        self.buffer_delta_index = 0
+        self.buffer_delta = buffer_delta_size * [0]
+        self.buffer_delta2_size = buffer_delta2_size
+        self.buffer_delta2_index = 0
+        self.buffer_delta2 = buffer_delta2_size * [0]
+
+        # ID which encoder
+        self.id = id
+
+        # Set up background counting of encoder pulse count
+        self.device.pin.when_changed = self.when_changed
+
+        # Set up and start background daemon thread that calculates delta and delta2 pulse counts every 'interval' seconds
+        self.thread = BackgroundTask(self.background_calc_pulse_delta, interval, self.id)
+        self.thread.start()
+        #print("Daemon = ", self.thread.daemon)
+ 
+    # Define method to run in background thread.
+    # Calculates delta and delta2 encoder pulse counts as proxy for speed and acceleration
+    def background_calc_pulse_delta(self):
+
+        # Calculate one delta pulse count
+        pc = self.pulse_count
+        self.pulse_delta = pc - self.pulse_last
+        self.pulse_delta2 = self.pulse_delta-self.pulse_delta_last
+        
+        # Store delta pulse count in smoothing delta buffer and move rotating index
+        self.buffer_delta[self.buffer_delta_index] = self.pulse_delta
+        self.buffer_delta_index = (self.buffer_delta_index + 1) % self.buffer_delta_size
+       #      print(self.id, self.pulse_delta, self.pulse_count, self.pulse_last)
+      
+        # Store delta2 pulse count in smoothing delta2 buffer and move rotating index
+        self.buffer_delta2[self.buffer_delta2_index] = self.pulse_delta2
+        self.buffer_delta2_index = (self.buffer_delta2_index + 1) % self.buffer_delta2_size
+
+        # Remember values for next call
+        self.pulse_last = pc
+        self.pulse_delta_last = self.pulse_delta
+ 
+    # Returns smoothed delta pulse count as property
+    @property
+    def smoothed_pulse_delta(self):
+        return sum(self.buffer_delta)
+    
+    # Returns smoothed delta2 pulse count as property
+    @property
+    def smoothed_pulse_delta2(self):
+        return sum(self.buffer_delta2)
+
+    # Define method called on detection of pulse count
+    def when_changed(self, time_ticks, state):
+        self.pulse_count += self.direction
+
+    # Defines direction we are going (1 = forward)
+    def set_direction(self, direction):
+        self.direction = direction
+
+    # Shutdown
+    def stop_all(self):
+        self.device.pin.when_changed = None
+        
+
+       
+
+ 
