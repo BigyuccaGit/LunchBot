192d400eec566a4381f0ae0828d962d5deb38ff9
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-11-16 15:14:12 +0000

    Suppressed encoders and Distance sensor: Exception handling added

--- server/spin_right.py
+++ server/spin_right.py
@@ -11,22 +11,22 @@ try:
     angle=90
     yaw=0.0
     speed = 100
-    while yaw < angle:
+    while -yaw < angle:
         sleep(0.001)
         dt,elapsed = delta.update()
         gyro=imu.read_gyroscope()
         yaw += gyro.z * dt
 
-        if angle - yaw > 30:
+        if angle + yaw > 30:
             speed = 100
         else:
             speed = 50
-    
-        robot.speeds(-speed, speed)
-            
+        
+        robot.speeds(speed, -speed)
+
 except KeyboardInterrupt:
     robot.stop()
-    
+
 finally:
     robot.stop()
 
