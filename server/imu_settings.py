d1c7fd62fa19a0853f76d6657199c81627e94b1c
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-11-20 19:20:18 +0000

    Modified gyro settings

--- server/imu_settings.py
+++ server/imu_settings.py
@@ -1,4 +1,4 @@
 from vpython import vector
 ##gyro_offsets = vector(-0.583969, 0.675573, -0.530534)
-gyro_offsets = vector(-0.6, -2.5, -0.04)
+gyro_offsets = vector(-0.69, -2.49, -0.11)
 mag_offsets = vector(-25.05, 6.075, 31.125)
