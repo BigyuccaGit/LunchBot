1fb86c3f30760d3e23e5c5eeef9f9dd38d7e79b7
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-11-30 21:10:14 +0000

    Apperently working

--- server/ObstacleAvoidance.py
+++ server/ObstacleAvoidance.py
@@ -1,10 +1,12 @@
 from robot import Robot, DistanceSensorNoEcho
 import atexit
 from time import sleep
+import time
 import random
 import logging
 import sys
-
+from icm20948 import ICM20948
+import math
 
 file_handler = logging.FileHandler(filename='robot.log',mode='w')
 stdout_handler = logging.StreamHandler(sys.stdout)
@@ -24,6 +26,10 @@ class ObstacleAvoidance:
         # Calcs for LEDs
         self.led_half = int(self.robot.leds.count/2)
         self.sense_colour = 255,0,0
+        self.imu = ICM20948()
+        self.acc_threshold = 3.0
+        self.acc_threshold_mag = self.acc_threshold * self.acc_threshold
+        self.collided = False
         
        # Ensure it will stop
         atexit.register(self.robot.stop_all)
@@ -38,18 +44,29 @@ class ObstacleAvoidance:
     # Create the 2 bars on the LED SHIM
     def display_state(self, left_distance, right_distance):
         # Clear first
-        self.robot.leds.clear()
+#        self.robot.leds.clear()
         # Left side
-        l_led_bar = self.distance_to_led_bar(left_distance)
-        self.robot.leds.set_range(range(l_led_bar), self.sense_colour)
+#        l_led_bar = self.distance_to_led_bar(left_distance)
+#        self.robot.leds.set_range(range(l_led_bar), self.sense_colour)
         # Right side
-        r_led_bar = self.distance_to_led_bar(right_distance)
-        start = (self.robot.leds.count - 1) - r_led_bar
-        logger.info(f"LEDS  {l_led_bar} {left_distance:.2f} {r_led_bar} {right_distance:.2f}")
-        self.robot.leds.set_range(range(start, self.robot.leds.count-1), self.sense_colour)
+#        r_led_bar = self.distance_to_led_bar(right_distance)
+#        start = (self.robot.leds.count - 1) - r_led_bar
+        #logger.info(f"LEDS  {l_led_bar} ({left_distance*100:.0f}) {r_led_bar} ({right_distance*100:.0f}) {collided}")
+        logger.info(f"TEST  ({left_distance*100:.0f}) ({right_distance*100:.0f})")
+
+#        self.robot.leds.set_range(range(start, self.robot.leds.count-1), self.sense_colour)
         # Now show
-        self.robot.leds.show()
-    
+#        self.robot.leds.show()
+
+    # Have we collided with something
+    def colliding(self):
+        if not self.collided:
+            ax, ay, az, _, _, _ = self.imu.read_accelerometer_gyro_data()
+            amag=ax*ax + ay*ay + az*az
+            self.collided = amag > self.acc_threshold_mag
+            if self.collided:
+                print("COLLIDED", amag)
+        return self.collided
         
     def backwards(self):
         self.robot.backward()
@@ -85,6 +102,9 @@ class ObstacleAvoidance:
 
         logging.basicConfig(level=logging.INFO, handlers=handlers, format='%(name)s %(levelname)s:%(asctime)s %(message)s')
         
+        acc_threshold = 3.0
+        dt=0.01
+        
         while True:
             
             # Send robot forward
@@ -92,18 +112,31 @@ class ObstacleAvoidance:
             self.robot.forward()
 
             # Wait for object to come in range
-            while not self.r_sensor.in_range and not self.l_sensor.in_range:
+            t=time.time()
+            inc=0
+            while (not self.r_sensor.in_range and not self.l_sensor.in_range) and (not self.colliding()) :
+            
+#            while (not self.r_sensor.in_range and not self.l_sensor.in_range):
+            
                 left_distance = self.l_sensor.distance
                 right_distance = self.r_sensor.distance
-                self.display_state(left_distance, right_distance)
-                sleep(0.01) 
+
+                inc += 1
+                if inc % 1 == 0:
+                    self.display_state(left_distance, right_distance)#, self.l_sensor.in_range, self.r_sensor.in_range )
+                t=t+dt
+                sleep(max(0, t-time.time()) ) 
 
             # Stop the robot
+            self.collided = False
             self.robot.stop()
             logger.info("STOP")
 
             # Choose which spin option to do in a moment based on current settings
-            self.spin_option=self.choose_spin(self.r_sensor.in_range, self.l_sensor.in_range)
+            if self.collided:
+                 self.spin_option=self.choose_spin(0, 0)
+            else:
+               self.spin_option=self.choose_spin(self.r_sensor.in_range, self.l_sensor.in_range)
 
             # Go Backwards
             logger.info("BACKWARD")
@@ -114,7 +147,7 @@ class ObstacleAvoidance:
             
             self.spin_option()    
 
-bot=Robot()
+bot=Robot(encoder=True)
 behaviour=ObstacleAvoidance(bot)
 behaviour.buzzer_enabled = False
 
